0d000674941c31755bbaae94d5a94946510d18e1
ynnako <ynnako.junk@gmail.com>
2020-04-17 18:23:09 +0300

    implemented some functions

--- ex1.cu
+++ ex1.cu
@@ -1,8 +1,26 @@
 #include "ex1.h"
 
+__device__ void create_histogram
+
 __device__ void prefix_sum(int arr[], int arr_size) {
-    return; // TODO
+    
+    int thIdx = threadIdx.x;
+    int inc;
+    for(int stride = 1 ; stride < blockDim.x; stride *= 2){
+        if(thIdx >= stride){
+            inc = arr[ thIdx - stride];
+        }    
+        __syncthreads();
+        if(thIdx >= stride){
+            arr[thIdx] += inc;
+        }
+        __syncthreads();
+    }
+    return; 
 }
+
+
+
 
 __global__ void process_image_kernel(uchar *all_in, uchar *all_out) {
     return; // TODO
@@ -11,6 +29,9 @@ __global__ void process_image_kernel(uchar *all_in, uchar *all_out) {
 /* Task serial context struct with necessary CPU / GPU pointers to process a single image */
 struct task_serial_context {
     // TODO define task serial memory buffers
+    uchar *gpu_in_img;
+    uchar *gpu_out_img;
+    int *gpu_g_hist;
 };
 
 /* Allocate GPU memory for a single input image and a single output image.
@@ -19,8 +40,10 @@ struct task_serial_context {
 struct task_serial_context *task_serial_init()
 {
     auto context = new task_serial_context;
-
     //TODO: allocate GPU memory for a single input image and a single output image
+    CUDA_CHECK( cudaMalloc(&context.gpu
+_in_img, IMG_HEIGHT * IMG_WIDTH) );
+    CUDA_CHECK( cudaMalloc(&context.gpu_out_img, IMG_HEIGHT * IMG_WIDTH) );
 
     return context;
 }
@@ -30,16 +53,27 @@ struct task_serial_context *task_serial_init()
 void task_serial_process(struct task_serial_context *context, uchar *images_in, uchar *images_out)
 {
     //TODO: in a for loop:
-    //   1. copy the relevant image from images_in to the GPU memory you allocated
-    //   2. invoke GPU kernel on this image
-    //   3. copy output from GPU memory to relevant location in images_out_gpu_serial
+    for(int i = 0 ; i < N_IMAGES ; i++){
+        //   1. copy the relevant image from images_in to the GPU memory you allocated
+        CUDA_CHECK( cudaMemcpy(context->gpu
+    _in_img , (images_in + i * IMG_HEIGHT * IMG_WIDTH) , IMG_HEIGHT * IMG_WIDTH, cudaMemcpyHostToDevice);
+       
+        //   2. invoke GPU kernel on this image  
+        ////////////TBD how many blocks and threads should we use?
+        process_image_kernel<<1,1>>(context->gpu_in_img , context->_out_img);
+        ////////////
+
+        //   3. copy output from GPU memory to relevant location in images_out_gpu_serial
+        CUDA_CHECK( cudaMemcpy((images_out + i * IMG_HEIGHT * IMG_WIDTH) , context->gpu_out_img , IMG_HEIGHT * IMG_WIDTH, cudaMemcpyDevicetToHost);
+    }
 }
 
 /* Release allocated resources for the task-serial implementation. */
 void task_serial_free(struct task_serial_context *context)
 {
     //TODO: free resources allocated in task_serial_init
-
+    CUDA_CHECK(cudaFree(context->gpu_in_img);
+    CUDA_CHECK(cudaFree(context->gpu_out_img);
     free(context);
 }
 
